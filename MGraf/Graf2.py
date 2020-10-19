eda73b169d488bcc4f1a325022769ebf956f655a
Vi <51240669+LethSphere@users.noreply.github.com>
2020-10-19 09:43:09 -0500

    Update Graf2.py

--- MGraf/Graf2.py
+++ MGraf/Graf2.py
@@ -6,11 +6,9 @@ import matplotlib.pyplot as plt
 # Construct lines
 # x > 0
 x = np.linspace(0, 20, 2000)
-# x1 >= 0
+
 y1 = 5 + x*0
 
-plt.xlabel(r'$x_2>=0$')
-plt.ylabel(r'$x_1>=0$')
 # Make plot
 plt.plot(x, y1)
 
