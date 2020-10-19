e893017d5ebd22e0f823ce86ef4ec33a8b038e9b
Vi <51240669+LethSphere@users.noreply.github.com>
2020-10-19 09:28:51 -0500

    Update graf1.py

--- MGraf/graf1.py
+++ MGraf/graf1.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 19 09:18:59 2020
-
-@author: User
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,12 +7,6 @@ import matplotlib.pyplot as plt
 x = np.linspace(-20, 20, 200)
 # x1 >= 0
 y1 = 5 - 2*x
-# 2x1+x2<=10
-y2 = 10-2*x
-# x2 >= 4x-8
-y3 = (4*x-8)/4.0
-# y <= 2x - 5 
-y4 = (5 * x +2)/2
 plt.xlabel(r'$x_2>=0$')
 plt.ylabel(r'$x_1>=0$')
 # Make plot
@@ -29,8 +16,6 @@ plt.ylim((0, 20))
 
 
 # Fill feasible region
-y5 = np.minimum(y2, y4)
-y6 = np.maximum(y1, y3)
 plt.fill_between(x, y1, color='purple', alpha=0.5)
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.grid()
